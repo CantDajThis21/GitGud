f3480f3d212a9f6de0587b082682b5fe3b5d8519
Dodgeruu <dodgebrianbongo@gmail.com>
2021-12-11 14:44:14 +0800

    added will cause merge conflict

--- gitsample.py
+++ gitsample.py
@@ -3,9 +3,6 @@ def sum(x,y):
 
 if __name__ == '__main__':
     x = 1
-    y = 2
+    y = 7
     s = sum(x, y)
     print ("Sum of two numbers are %s", s)
-    print ("x is %s", x)
-    print ("y is %s", y)
-
